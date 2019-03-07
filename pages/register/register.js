6e59af4166c3781e500c3c0188bfa2d195fd5b77
littleLane <littleLaneBye@gmail.com>
2019-03-07 23:51:27 +0800

    点击注册逻辑

--- pages/register/register.js
+++ pages/register/register.js
@@ -76,6 +76,21 @@ Page({
     }
   },
 
+  // 点击注册
+  handleRegister() {
+    const { mobile, vercode } = this.data;
+    
+    if (!mobile) {
+      this.showToast('请输入手机号');
+    } else if (!MOBILE_REG.test(mobile)) {
+      this.showToast('请输入合法的手机号');
+    } else if (sixCodeRandom !== vercode) {
+      this.showToast('请输入正确的验证码');
+    } else {
+      
+    }
+  },
+
   // 显示提示信息
   showToast(title, duration = 2000) {
     wx.showToast({
