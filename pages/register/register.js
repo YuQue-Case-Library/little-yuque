d52ab64f1bb8543c0204ca4fc7b72efd2b82df30
littleLane <littleLaneBye@gmail.com>
2019-03-11 14:40:31 +0800

    注册逻辑拆分，与完善

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,5 +1,4 @@
-const { MOBILE_REG } = require('../../utils/constants.js');
-const { getSixCodeRandom, countdown } = require('../../utils/util.js');
+const { MOBILE_REG } = require('../../static/utils/constants.js');
 
 Page({
   data:{
@@ -9,55 +8,6 @@ Page({
     isCodeLimit: false,
     codeInputFocus: false,
     sendCodeMessage: '',
-  },
-
-  // 监听手机输入框事件
-  bindMobileInput(e) {
-    this.setData({
-      mobile: e.detail.value
-    });
-  },
-
-  // 获取验证码
-  getVerCode() {
-    const { mobile, isCodeLimit } = this.data;
-    const resetTime = 60;
-    let sixCodeRandom = '';
-
-    if (!isCodeLimit) {
-      if (!mobile) {
-        this.showToast('请输入手机号');
-      } else if (!MOBILE_REG.test(mobile)) {
-        this.showToast('请输入合法的手机号');
-      } else {
-        sixCodeRandom = getSixCodeRandom();
-        const timer = setTimeout(() => {
-          clearTimeout(timer);
-
-          this.setData({
-            sendCodeMessage: `获取验证成功，您获取的验证码是 ${sixCodeRandom}`,
-            codeInputFocus: true
-          });
-
-          countdown({
-            count: resetTime,
-            rate: 1000,
-            callback: currentTime => {
-              this.setData({
-                isCodeLimit: true,
-                codeBtnText: `${currentTime}s`
-              });
-            },
-            finishCallback: () => {
-              this.setData({
-                isCodeLimit: false,
-                codeBtnText: '重发验证码'
-              });
-            }
-          });
-        }, 800);
-      }
-    }
   },
 
   // 点击注册
