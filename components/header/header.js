60d39d7fdb4205d9a7953cfab739d34c9d5123cc
littleLane <littleLaneBye@gmail.com>
2019-03-14 20:15:53 +0800

    header 添加菜单逻辑

--- components/header/header.js
+++ components/header/header.js
@@ -1,3 +1,15 @@
 Component({
-  
+  data: {
+    isExpand: false
+  },
+  methods: {
+    handleExpand() {
+      const { isExpand } = this.data;
+      this.setData({
+        isExpand: !isExpand
+      });
+
+      this.triggerEvent('scrollEvent', !isExpand);
+    }
+  }
 })
