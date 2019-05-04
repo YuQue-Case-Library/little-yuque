9cdbac9bb80ec759760767be08e2f0d88c798484
littleLane <littleLaneBye@gmail.com>
2019-05-04 20:10:43 +0800

    feat: 利用 token 登录

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,46 +1,30 @@
-const app = new getApp();
+const app = new getApp()
 
 Page({
-  data:{
-    // 表示登录方式  1 => 密码登录   2 => 验证码登录
-    loginType: 1
-  },
-
-  // 切换登录方式
-  handleSwitchLogin() {
-    const { loginType } = this.data;
-
-    if (loginType === 1) {
-      this.setData({
-        loginType: 2
-      });
-    } else if (loginType === 2) {
-      this.setData({
-        loginType: 1
-      });
-    }
-  },
-
   // 提交表单
   formSubmit(e) {
-    const loginData = e.detail.value;
-    const { username, password } = loginData;
+    const { userToken } = e.detail.value
 
-    if(username !== '' && password !== '') {
-      this.login(loginData);
+    if(userToken !== '') {
+      this.login({ userToken })
     }
   },
 
   // 登录
-  login(loginData) {
-    app.globalData.$api({
-      url: `/users/${loginData.username}`,
-      success({ data }) {
-        wx.setStorage({
-          key: 'userInfo',
-          data: data.data,
-          success(res) {
-            
+  login({ userToken }) {
+    wx.setStorage({
+      key: 'userToken',
+      data: userToken,
+      success() {
+        app.globalData.$api({
+          url: '/user',
+          success({ data }) {
+            if (data && data.data) {
+              wx.setStorage({
+                key: 'userInfo',
+                data: data.data,
+              })
+            }
           }
         })
       }
