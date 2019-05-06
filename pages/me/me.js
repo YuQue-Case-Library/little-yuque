06400d86658642e9edc98dc809385636c7fc58ad
littleLane <littleLaneBye@gmail.com>
2019-05-06 17:28:30 +0800

    feat：我的个人页面功能完善

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,33 +1,130 @@
-const app = new getApp()
+const { HOME_PAGE_PREFIX } = require('../../static/utils/constants.js');
 
-Page({
-  // 提交表单
-  formSubmit(e) {
-    const { userToken } = e.detail.value
-
-    if(userToken !== '') {
-      this.login({ userToken })
-    }
+Component({
+  data: {
+    avatarUrl: '',
+    nickName: '',
+    description: '',
+    following: 0,
+    followers: 0,
+    homePageUrl: '',
+    userToken: ''
   },
 
-  // 登录
-  login({ userToken }) {
-    wx.setStorage({
+  ready() {
+    // 获取当前用户的 token
+    wx.getStorage({
       key: 'userToken',
-      data: userToken,
-      success() {
-        app.globalData.$api({
-          url: '/user',
-          success({ data }) {
-            if (data && data.data) {
-              wx.setStorage({
-                key: 'userInfo',
-                data: data.data,
-              })
-            }
-          }
+      success: res => {
+        this.setData({
+          userToken: res.data
         })
-      }
+      },
+    })
+
+    // 获取用户信息
+    wx.getStorage({
+      key: 'userInfo',
+      success: res => {
+        const userInfo = res.data 
+        this.setData({
+          avatarUrl: userInfo.avatar_url,
+          nickName: userInfo.name,
+          description: userInfo.description,
+          following: userInfo.following_count,
+          followers: userInfo.followers_count,
+          homePageUrl: `${HOME_PAGE_PREFIX}${userInfo.name}`
+        })
+      },
     })
   },
+
+  methods: {
+    // 点击复制数据
+    handleClipboardData(event) {
+      const { clipType } = event.currentTarget.dataset
+
+      if (clipType) {
+        const clipData = this.data[clipType]
+        if (clipData) {
+          wx.setClipboardData({
+            data: clipData,
+            success() {
+              wx.showToast({
+                title: '复制数据成功!',
+                icon: 'success',
+                duration: 1500
+              })
+            }
+          })
+        }
+      }
+    },
+
+    // 清除缓存数据
+    handleClearEvent() {
+      wx.showModal({
+        title: '提示',
+        content: '是否确定清空本地缓存数据',
+        success: res => {
+          if (res.confirm) {
+            this.handleClear()
+          }
+        }
+      })
+    },
+  
+    handleClear(callback) {
+      wx.removeStorage({
+        key: 'userToken',
+        success() {
+          wx.removeStorage({
+            key: 'userInfo',
+            success() {
+              if (callback) {
+                callback()
+                return
+              }
+              wx.showToast({
+                title: '缓存数据清空成功',
+                icon: 'success',
+                duration: 1500
+              })
+            },
+            fail() {
+              wx.showToast({
+                title: '缓存数据清空失败',
+                icon: 'fail',
+                duration: 1500
+              })
+            }
+          })
+        },
+        fail() {
+          wx.showToast({
+            title: '缓存数据清空失败',
+            icon: 'fail',
+            duration: 1500
+          })
+        }
+      })
+    },
+
+    // 退出登录
+    handleLogout() {
+      wx.showModal({
+        title: '提示',
+        content: '是否确定清空缓存数据并退出登录',
+        success: res => {
+          if (res.confirm) {
+            this.handleClear(() => {
+              wx.redirectTo({
+                url: 'pages/login/login'
+              })
+            })
+          }
+        }
+      })
+    }
+  }
 })
