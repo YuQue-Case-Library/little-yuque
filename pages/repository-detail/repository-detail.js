a0c2aaf31b3b6c3f1b01823d68150a72087f6a79
littleLane <littleLaneBye@gmail.com>
2019-05-10 14:20:26 +0800

    feat: 知识库详情页

---
+++ pages/repository-detail/repository-detail.js
@@ -0,0 +1,27 @@
+const app = new getApp()
+
+Page({
+  data:{
+  },
+  onLoad:function(options){
+    // 生命周期函数--监听页面加载
+  },
+  onReady:function(){
+    
+  },
+  onShow:function(){
+    // 生命周期函数--监听页面显示
+  },
+  onHide:function(){
+    // 生命周期函数--监听页面隐藏
+  },
+  onUnload:function(){
+    // 生命周期函数--监听页面卸载
+  },
+  onPullDownRefresh: function() {
+    // 页面相关事件处理函数--监听用户下拉动作
+  },
+  onReachBottom: function() {
+    // 页面上拉触底事件的处理函数
+  },
+})
