703e52b36456a684377a7ccfb55b08d6c62bf906
littleLane <littleLaneBye@gmail.com>
2019-05-10 14:41:46 +0800

    feat: tab 切换优化

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -1,28 +1,28 @@
 Page({
   data:{
-    tabActive: 1
-  },
-  onLoad:function(options){
-    console.log('onLoad')
-    // 生命周期函数--监听页面加载
-  },
-  onReady:function(){
-    // 生命周期函数--监听页面初次渲染完成
-  },
-  onShow:function(){
-    // 生命周期函数--监听页面显示
-  },
-  onHide:function(){
-    // 生命周期函数--监听页面隐藏
-  },
-  onUnload:function(){
-    // 生命周期函数--监听页面卸载
-  },
-  onPullDownRefresh: function() {
-    // 页面相关事件处理函数--监听用户下拉动作
-  },
-  onReachBottom: function() {
-    // 页面上拉触底事件的处理函数
+    tabActive: 'document',
+    tabData: [
+      {
+        icon: 'file',
+        value: 'document',
+        text: '文档'
+      },
+      {
+        icon: 'storehouse',
+        value: 'repository',
+        text: '知识库'
+      },
+      {
+        icon: 'team',
+        value: 'group',
+        text: '团队'
+      },
+      {
+        icon: 'user-h',
+        value: 'me',
+        text: '我的'
+      }
+    ]
   },
 
   // 切换 footer tab
@@ -33,7 +33,7 @@ Page({
     }
     
     this.setData({
-      tabActive: e.currentTarget.dataset.index
+      tabActive: currentActive
     })
   }
 })
