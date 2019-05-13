150fec25fd8d863f49a81b2e9e19db559a86f646
littleLane <littleLaneBye@gmail.com>
2019-05-13 22:35:15 +0800

    feat: 新增知识库

--- pages/repository-add/repository-add.js
+++ pages/repository-add/repository-add.js
@@ -2,24 +2,116 @@ const app = new getApp()
 
 Page({
   data: {
-    
+    userInfo: {},
+    ownerGroups: [],
+    selectedGroupIndex: 0,
+
+    repoTypes: [
+      {
+        id: "Book",
+        name: "文档"
+      },
+      {
+        id: "Book",
+        name: "专栏"
+      },
+      {
+        id: "Design",
+        name: "画板"
+      }
+    ],
+    selectTypeIndex: 0,
+
+    publicArray: [
+      {
+        id: 0,
+        name: "仅自己可见"
+      },
+      {
+        id: 2,
+        name: "互联网可见"
+      }
+    ],
+    selectPublicIndex: 0,
+  },
+
+  onReady: function() {
+    wx.getStorage({
+      key: "userInfo",
+      success: res => {
+        const userInfo = res.data
+
+        this.setData({
+          userInfo
+        })
+
+        if (userInfo && userInfo.id) {
+          app.globalData.$api({
+            url: `/users/${userInfo.login}/groups`,
+            success: ({ data }) => {
+              if (data.code === "validation") {
+                wx.showToast({
+                  title: data.message,
+                  icon: "none",
+                  duration: 2000
+                })
+              } else if (Array.isArray(data.data)) {
+                this.setData({
+                  ownerGroups: [
+                    {
+                      id: userInfo.id,
+                      name: userInfo.name
+                    }
+                  ].concat(
+                    data.data.map(group => ({
+                      ...group,
+                      name: `（团队）${group.name}`
+                    }))
+                  )
+                })
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+
+  handleTypePick(e) {
+    this.setData({
+      selectTypeIndex: e.detail.value
+    })
+  },
+
+  handlePublicPick(e) {
+    this.setData({
+      selectPublicIndex: e.detail.value
+    })
+  },
+
+  handleGroupPick(e) {
+    this.setData({
+      selectedGroupIndex: e.detail.value
+    })
   },
 
   // 提交表单
   formSubmit(e) {
-    const { name, description } = e.detail.value
+    const formData = e.detail.value
+    const { name, description } = formData
     const nameTrim = name.trim()
 
-    if (nameTrim === '') {
+    if (nameTrim === "") {
       wx.showToast({
-        title: '请填写团队名称',
-        icon: 'none',
+        title: "请填写团队名称",
+        icon: "none",
         duration: 2000
       })
     } else {
       this.handleCreateGroup({
+        ...formData,
         name: nameTrim,
-        description: description.trim(),
+        description: description.trim()
       })
     }
   },
@@ -27,24 +119,41 @@ Page({
   // 创建团队
   handleCreateGroup(dataObj) {
     wx.getStorage({
-      key: 'userInfo',
+      key: "userInfo",
       success: res => {
         const userInfo = res.data
+        const { publicArray, selectPublicIndex, repoTypes, selectTypeIndex, ownerGroups, selectedGroupIndex } = this.data
+        const currentGroup = ownerGroups[selectedGroupIndex]
+        const groupLogin = currentGroup.login || currentGroup.name
 
         if (userInfo && userInfo.id) {
           app.globalData.$api({
-            url: '/groups',
-            method: 'post',
+            url: "/groups",
+            method: "post",
             data: {
               ...dataObj,
-              login: userInfo.login
+              public: publicArray[selectPublicIndex].id,
+              type: repoTypes[selectTypeIndex].id,
+              login: dataObj.book_slug,
+              namespace: `${groupLogin}\/${dataObj.book_slug}`
             },
             success({ data }) {
               if (data.code === "validation") {
                 wx.showToast({
                   title: data.message,
-                  icon: 'none',
+                  icon: "none",
                   duration: 2000
+                })
+              } else {
+                wx.showToast({
+                  title: '创建知识库成功',
+                  icon: 'success',
+                  duration: 2000,
+                  success: () => {
+                    wx.redirectTo({
+                      url: '/pages/repository/repository'
+                    })
+                  }
                 })
               }
             }
