a358df2c917105daef1d1261a4493bba556b095d
littleLane <littleLaneBye@gmail.com>
2019-05-14 14:32:10 +0800

    feat: 添加生成 hash 方法

--- static/utils/util.js
+++ static/utils/util.js
@@ -41,9 +41,25 @@ const showSimpleToast = (title, duration = 2000) => {
   });
 }
 
+function gethashcode (hashLength = 6) {
+  if (!hashLength || typeof(Number(hashLength)) != 'number') return;
+
+  var ar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  var hs = [];
+  var hl = Number(hashLength);
+  var al = ar.length;
+  
+  for (var i = 0; i < hl; i ++) {
+    hs.push(ar[Math.floor(Math.random() * al)]);
+  }
+   
+  return hs.join('');
+}
+
 module.exports = {
   formatTime,
   getSixCodeRandom,
   countdown,
-  showSimpleToast
+  showSimpleToast,
+  gethashcode
 }
